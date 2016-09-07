077c4a0f2346d04a3ce97174dd98638ebe22bd4d
Luis Felipe Pimentel Benassi <luisfelipebenassi@gmail.com>
2016-09-07 03:07:30 -0300

    Create folders with a root default name

--- lib/cukestart/generator/structure.rb
+++ lib/cukestart/generator/structure.rb
@@ -3,12 +3,19 @@ require 'thor'
 module Cukestart
   module Generator
     class Structure < Thor::Group
+      include Thor::Actions
       argument :project_name, :desc => "Name of the root directory", :default => "teste_gem"
 
       def self.source_root
         File.dirname(__FILE__) + :project_name
       end
 
+      def create_folders
+        empty_directory(project_name)
+        empty_directory("#{project_name}/features")
+        empty_directory("#{project_name}/features/support")
+        empty_directory("#{project_name}/features/step_definitions")
+      end
     end
   end
 end
