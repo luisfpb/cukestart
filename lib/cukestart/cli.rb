57cff7a13ba50b21815f8ec70125a40f2f7941f0
Luis Benassi <luis.benassi@walmart.com>
2016-09-14 12:01:42 -0300

    Receive the project name

---
+++ lib/cukestart/cli.rb
@@ -0,0 +1,11 @@
+require 'thor'
+require 'cukestart/generator/structure'
+
+module Cukestart
+  class Cli < Thor
+    desc "project <project_name>", "Create your project"
+    def project(project_name)
+      Cukestart::Generator::Structure.start([porject_name])
+    end
+  end
+end
