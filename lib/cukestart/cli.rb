76d0b9016e1c35ead8ee5a6fe72326076d3f5b35
Luis Benassi <luis.benassi@walmart.com>
2016-09-14 17:55:56 -0300

    add cucumber yml

--- lib/cukestart/cli.rb
+++ lib/cukestart/cli.rb
@@ -5,7 +5,7 @@ module Cukestart
   class Cli < Thor
     desc "project <project_name>", "Create your project"
     def project(project_name)
-      Cukestart::Generator::Structure.start([porject_name])
+      Cukestart::Generator::Structure.start([project_name])
     end
   end
 end
--- lib/cukestart/generator/structure.rb
+++ lib/cukestart/generator/structure.rb
@@ -7,7 +7,7 @@ module Cukestart
       argument :project_name, :desc => "Name of the root directory"
 
       def self.source_root
-        File.dirname(__FILE__) + :project_name
+        File.dirname(__FILE__)
       end
 
       def create_folders
@@ -16,6 +16,13 @@ module Cukestart
         empty_directory("#{project_name}/features/support")
         empty_directory("#{project_name}/features/step_definitions")
       end
+
+      def generate_cucumber_yml
+        template('templates/cucumber.tt', "#{project_name}/cucumber.yml")
+      end
+
+
+
     end
   end
 end
