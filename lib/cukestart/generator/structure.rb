435c70e9290698680fd9132abc18ae3864985900
Luis Benassi <luis.benassi@walmart.com>
2016-09-26 18:29:21 -0300

    copy and generate files on start

--- lib/cukestart/generator/structure.rb
+++ lib/cukestart/generator/structure.rb
@@ -5,6 +5,8 @@ module Cukestart
     class Structure < Thor::Group
       include Thor::Actions
       argument :project_name, :desc => "Name of the root directory"
+      argument :front, :desc => "If it will be a front test project", :default => true
+      argument :page_object, :desc => "Will use page_object", :default => true
 
       def self.source_root
         File.dirname(__FILE__)
@@ -15,10 +17,23 @@ module Cukestart
         empty_directory("#{project_name}/features")
         empty_directory("#{project_name}/features/support")
         empty_directory("#{project_name}/features/step_definitions")
+        empty_directory("#{project_name}/features/pages") if page_object & front
       end
 
       def copy_cucumber_yml
         copy_file('files/cucumber.yml', "#{project_name}/cucumber.yml")
+      end
+
+      def create_gemfile
+        template('templates/gemfile.tt', "#{project_name}/Gemfile")
+      end
+
+      def create_env
+        template('templates/env.tt', "#{project_name}/features/support/env.rb")
+      end
+
+      def create_capybara
+        template('templates/capybara.tt', "#{project_name}/features/support/capybara.rb")
       end
 
     end
