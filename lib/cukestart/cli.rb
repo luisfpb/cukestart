fab54e20d9c52930ad716d3beb67531f9ab6f1a9
Luis Felipe Pimentel Benassi <luis.benassi@walmart.com>
2016-12-01 17:24:49 -0200

    fix allias

--- lib/cukestart/cli.rb
+++ lib/cukestart/cli.rb
@@ -5,8 +5,8 @@ module Cukestart
   class Cli < Thor
 
     desc "project <project_name>", "Create your project"
-    method_option :front, :desc => "If it will be a front test project", :type => :boolean, :aliases => "-f"
-    method_option :page_object, :desc => "Will use page_object", :type => :boolean, :aliases => "-po"
+    method_option :front, :desc => "If it will be a front test project", :type => :boolean, :aliases => "f"
+    method_option :page_object, :desc => "Will use page_object", :type => :boolean, :aliases => "p"
 
     def project(project_name)
       Cukestart::Generator::Structure.start([project_name, options[:front], options[:page_object]])
